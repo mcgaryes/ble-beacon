5f9e7493d8d8f4d2924aa983c80ce2ef57654d7b
Eric McGary <emcgary@resource.com>
2013-10-16 12:36:03 -0400

    Initial commit

---
+++ BLEBeacon/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BLEBeacon
+//
+//  Created by Eric McGary on 10/16/13.
+//  Copyright (c) 2013 Eric McGary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
